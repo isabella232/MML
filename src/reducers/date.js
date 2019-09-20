510b8b1b50da5624ecb8c5d1107d284a176bcc2f
Cezary <ziemlicki@gmail.com>
2019-09-20 11:45:35 +0200

    Added Copyright header

--- src/reducers/date.js
+++ src/reducers/date.js
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2018 - 2019 Orange
+ * 
+ * This software is distributed under the terms and conditions of the 'MIT'
+ * license which can be found in the file 'LICENSE.txt' in this package distribution 
+ * or at https://spdx.org/licenses/MIT
+ *
+ */
+
+ /* Orange contributed module for use on CozyCloud platform
+ * 
+ * Module name: MML - Mapping My Life
+ * Version:     1.0.5
+ * Created:     2018 by Orange
+ */
+
+
 import {
   FETCH_GEO_LAST_DAY,
   SELECT_DATE
